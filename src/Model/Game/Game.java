1c9b74ad442010e33640828c45edf802bb04a870
Semakon <Semakon@gmail.com>
2016-01-24 19:01:50 +0100

    Changed Game

--- src/Model/Game/Game.java
+++ src/Model/Game/Game.java
@@ -1,9 +1,11 @@
 package Model.Game;
 
+import Model.Game.Exceptions.InsufficientTilesInPoolException;
 import Model.Player.Player;
+import Model.Player.SocketPlayer;
 
 /**
- * Created by martijn on 11-1-16.
+ * Created by Martijn on 11-1-16.
  */
 public class Game {
 
@@ -11,10 +13,30 @@ public class Game {
     private Player[] players;
     private int currentPlayer;
 
-    public Game(String[] players, String firstMove) {
+    public Game(String thisPlayer, String[] players, String firstMove) {
         board = new Board();
         this.players = new Player[players.length];
-        //TODO: create players
+        if (thisPlayer == null) {
+            //Game on Server.
+            for (int i = 0; i < players.length; i++) {
+                try {
+                    Player p = new SocketPlayer(players[i], board.getPool().takeTiles(6));
+                    this.players[i] = p;
+                } catch (InsufficientTilesInPoolException e) {
+                    //should not occur
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            //Game on Client
+            //TODO: implement
+        }
+        for (int i = 0; i < this.players.length; i++) {
+            if (this.players[i].getName().equals(firstMove)) {
+                currentPlayer = i;
+                break;
+            }
+        }
     }
 
     public Board getBoard() {
