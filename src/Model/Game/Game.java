e54134aed2b044b7a2b57d6b3b9777ebea64121f
martijn <mpdevries153@gmail.com>
2016-01-25 14:42:24 +0100

    Implemented Game partially. Added SocketPlayer

--- src/Model/Game/Game.java
+++ src/Model/Game/Game.java
@@ -1,6 +1,9 @@
 package Model.Game;
 
+import Model.Game.Exceptions.InsufficientTilesInPoolException;
+import Model.Player.HumanPlayer;
 import Model.Player.Player;
+import Model.Player.SocketPlayer;
 
 /**
  * Created by martijn on 11-1-16.
@@ -11,10 +14,34 @@ public class Game {
     private Player[] players;
     private int currentPlayer;
 
-    public Game(Player[] players, int firstMove) {
+    /**
+     * Creates an instance of Game. This can be on a Server or on a Client and the behaviour changes accordingly.
+     * @param thisPlayer The player of this client. Null if this instance of Game runs on a Server. Is not present in players.
+     * @param players Array of names of players. Does not contain thisPlayer.
+     * @param firstMove Name of player with the first move.
+     * @throws InsufficientTilesInPoolException
+     */
+    public Game(String thisPlayer, String[] players, String firstMove) throws InsufficientTilesInPoolException {
         board = new Board();
-        this.players = players;
-        this.currentPlayer = firstMove;
+        if (thisPlayer == null) {
+            //Game is on Server
+            this.players = new SocketPlayer[players.length];
+            for (int i = 0; i < this.players.length; i++) {
+                this.players[i] = new SocketPlayer(players[i], board.getPool().takeTiles(Configuration.HAND));
+            }
+        } else {
+            //Game is on Client
+            this.players = new Player[players.length + 1];
+            this.players[0] = new HumanPlayer(thisPlayer, board.getPool().takeTiles(Configuration.HAND));
+            for (int i = 0; i < this.players.length; i++) {
+                this.players[i] = new SocketPlayer(players[i], board.getPool().takeTiles(Configuration.HAND));
+            }
+        }
+        for (int i = 0; i < this.players.length; i++) {
+            if (this.players[i].getName().equals(firstMove)) {
+                currentPlayer = i;
+            }
+        }
     }
 
     public Board getBoard() {
---
+++ src/Model/Player/SocketPlayer.java
@@ -0,0 +1,39 @@
+package Model.Player;
+
+import Model.Game.*;
+import Model.Game.Exceptions.InvalidMoveException;
+
+import java.util.List;
+
+/**
+ * Created by martijn on 25-1-16.
+ */
+public class SocketPlayer extends Player {
+    /**
+     * Creates a new instance of Player with a name and a hand, that contains exactly Configuration.HAND Tiles.
+     *
+     * @param name The player's name.
+     * @param hand The player's Tiles.
+     */
+    public SocketPlayer(String name, List<Tile> hand) {
+        super(name, hand);
+    }
+
+    @Override
+    public PutMove determinePutMove(Board board) {
+        //Get move from server.
+        return null;
+    }
+
+    @Override
+    public TradeMove determineTradeMove(Board board) {
+        //Get move from server.
+        return null;
+    }
+
+    @Override
+    public Move determineMove(Board board) throws InvalidMoveException {
+        //Get choice from server.
+        return null;
+    }
+}
