83ee88a69ab667b091fafd46658a685db44734a6
Semakon <Semakon@gmail.com>
2016-01-31 23:16:14 +0100

    tests

--- src/Tests/TileTest.java
+++ src/Tests/TileTest.java
@@ -12,16 +12,19 @@ public class TileTest {
         System.out.println(tile1.getId());
         System.out.println(tile1.getColor());
         System.out.println(tile1.getShape());
+        System.out.println(tile1 + "\n");
 
         Tile tile2 = new Tile(32);
         System.out.println(tile2.getId());
         System.out.println(tile2.getColor());
         System.out.println(tile2.getShape());
+        System.out.println(tile2 + "\n");
 
         Tile tile3 = new Tile(20);
         System.out.println(tile3.getId());
         System.out.println(tile3.getColor());
         System.out.println(tile3.getShape());
+        System.out.println(tile3);
     }
 
 }
